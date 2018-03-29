72ea246acd492f6f9d5bdd17c52e4690892bbf43
OIdiotLin <lhd1997@qq.com>
2018-03-29 12:54:11 +0800

    MVVM demo

---
+++ app/src/main/java/com/lmmnb/guangzhouculturehelper/app/view/activity/DemoActivity.java
@@ -0,0 +1,36 @@
+package com.lmmnb.guangzhouculturehelper.app.view.activity;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.lmmnb.guangzhouculturehelper.R;
+import com.lmmnb.guangzhouculturehelper.app.viewModel.TranslateViewModel;
+import com.lmmnb.guangzhouculturehelper.databinding.ActivityDemoBinding;
+
+public class DemoActivity extends AppCompatActivity {
+
+    private TranslateViewModel translateVM;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 注意此处用 DataBindingUtil.setContentView 替换缺省的布局函数，
+        // 这里返回了 dataBinding 类，用于做数据绑定。
+        ActivityDemoBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_demo);
+
+        // View - DemoActivity 拥有一个
+        translateVM = new TranslateViewModel(this);
+
+        binding.setVm(translateVM);
+
+    }
+
+
+    public void onRefreshClick(View view) {
+        translateVM.getDemo();
+    }
+
+}
---
+++ app/src/main/java/com/lmmnb/guangzhouculturehelper/app/viewModel/TranslateViewModel.java
@@ -0,0 +1,54 @@
+package com.lmmnb.guangzhouculturehelper.app.viewModel;
+
+import android.content.Context;
+import android.databinding.BaseObservable;
+
+import com.lmmnb.guangzhouculturehelper.api.apis.Language;
+import com.lmmnb.guangzhouculturehelper.api.model.TranslateModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class TranslateViewModel extends BaseObservable {
+
+    private Context context;
+    private TranslateModel translate;
+
+    private List<String> demo;
+
+    public TranslateViewModel(Context context) {
+        this.context = context;
+
+        // Demo data
+        demo = new ArrayList<>();
+        demo.add("你是谁？");
+        demo.add("你要去哪儿？");
+        demo.add("我觉得你说的对。");
+        demo.add("对不起，是我错了。");
+        demo.add("连学长太厉害了。");
+
+    }
+
+    public void getDemo() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                translate = Language.translateToCantoneseByText(demo.get(new Random().nextInt(demo.size())));
+
+                // 当数据修改后，一定要显式调用 notifyChange()。
+                // 当然你也可以实现一个 Interface ，在 interface 中用 Callback 的方式做 notifyChange()
+                notifyChange();
+            }
+        }).start();
+    }
+
+    public TranslateModel getTranslate() {
+        return translate;
+    }
+
+    public void setTranslate(TranslateModel translate) {
+        this.translate = translate;
+    }
+
+}
